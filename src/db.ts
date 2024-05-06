2bdeaa3bc40f8b0a42ef1292387ea0ef8b227c66
Keith Whitson <keith.whitson@franka.de>
2024-05-06 19:11:42 +0200

    made changes to db

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,5 @@
-import Dexie from 'dexie';
+import Dexie from 'dexie'; // Add the missing import statement for 'dexie' package
+
 import countries_list from '../src/countries.json';
 
 interface Country {
@@ -20,8 +21,8 @@ interface Country {
 class MyDatabase extends Dexie {
   newcountries!: Dexie.Table<Country, number>; // string is the type of the primary key
   constructor() {
-    super("DishDatabase_2");
-    this.version(13).stores({
+    super("DishDatabase");
+    this.version(39).stores({
       newcountries: '++id, name, &code, title, dish, overview, originsandcreation, culturalsignificance, whatmakesitnationaldish,  instructions, ingredients, imagelink',
     });
   }
@@ -29,13 +30,30 @@ class MyDatabase extends Dexie {
 
 const db = new MyDatabase();
 
+db.version(39).stores({
+  newcountries: '++id, name, &code, title, dish, overview, originsandcreation, culturalsignificance, whatmakesitnationaldish,  instructions, ingredients, imagelink'
+}).upgrade(async () => {
+  await db.newcountries.clear();
+  const formattedCountriesList = countries_list.map(country => ({
+    name: country.Name,
+    code: country.Code,
+    title: country.Title,
+    dish: country.Dish,
+    overview: country.Overview,
+    originsandcreation: country.OriginsAndCreation,
+    culturalsignificance: country.CulturalSignificance,
+    whatmakesitnationaldish: country.WhatMakesItNationalDish,
+    instructions: country.Instructions,
+    ingredients: country.Ingredients,
+    imagelink: country.ImageLink
+  }));
+  await db.newcountries.bulkAdd(formattedCountriesList);
+});
 
-
-
-  db.version(37).stores({
-    newcountries: '++id, name, &code, title, dish, overview, originsandcreation, culturalsignificance, whatmakesitnationaldish,  instructions, ingredients, imagelink'
-  }).upgrade(async (transaction) => {
-    await db.newcountries.clear();
+// Function to load data into the database if empty
+async function loadDataIfEmpty() {
+  const count = await db.newcountries.count();
+  if (count === 0) { // Check if the table is empty
     const formattedCountriesList = countries_list.map(country => ({
       name: country.Name,
       code: country.Code,
@@ -50,7 +68,14 @@ const db = new MyDatabase();
       imagelink: country.ImageLink
     }));
     await db.newcountries.bulkAdd(formattedCountriesList);
-  });
+    console.log('Data loaded as the database was empty.');
+    window.location.reload(); // Reload the application
+  } else {
+    console.log('Database already has data.');
+  }
+}
 
+// Call the function to load data if the database is empty
+loadDataIfEmpty();
 
 export default db;
